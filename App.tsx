6e0d2e3f2b0d16560a6e8952710ef0a9a9950b94
digitropical <digitropical@outlook.com>
2026-02-03 13:16:57 -0500

    removed sticky button from mobile

--- App.tsx
+++ App.tsx
@@ -48,16 +48,6 @@ const App: React.FC = () => {
             </main>
 
             <Footer />
-
-            {/* Mobile Sticky CTA */}
-            <div className="md:hidden fixed bottom-0 left-0 right-0 z-50 p-4 bg-gradient-to-t from-[#121212] to-transparent pointer-events-none">
-                <a 
-                    href="#contact" 
-                    className="pointer-events-auto block w-full py-4 bg-[#d4af37] text-[#121212] text-center text-xs tracking-widest font-bold uppercase shadow-[0_-10px_20px_rgba(0,0,0,0.5)]"
-                >
-                    Book Repair Bench
-                </a>
-            </div>
         </div>
     );
 };
